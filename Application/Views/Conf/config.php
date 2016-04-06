be2cd2c15a090f355c79986e4a9a7c51c9ae7a18
zhai13 <4254001@qq.com>
2016-04-06 23:36:09 +0800

    三（2）：视图变量赋值和渲染
    1、使用控制器的assign方法为视图变量赋值：assign('name', $value)；
    2、渲染视图（控制器display、fetch和show方法）
    3、display('模板文件',[,'字符编码'][,'输出类型'])
    4、fetch('模板文件',[,'字符编码'][,'输出类型'])会返回渲染后的内容，但不会输出
    5、show('渲染内容',[,'字符编码'][,'输出类型'])输出，一般是配合fetch方法使用

--- Application/Views/Conf/config.php
+++ Application/Views/Conf/config.php
@@ -1,8 +1,8 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-    'DEFAULT_V_LAYER' => 'Template',    //自定义默认视图层
-    'TMPL_TEMPLATE_SUFFIX' => '.zhai',    //自定义模板文件后缀
-    'TMPL_FILE_DEPR' => '_',    //连接符（防止模板目录太深,模板文件格式：'控制器名_方法名.模板后缀'）
-    'VIEW_PATH' => './Theme/Views/',    //自定义视图目录：自定义模块视图文件目录
+    //'DEFAULT_V_LAYER' => 'Template',    //自定义默认视图层
+    //'TMPL_TEMPLATE_SUFFIX' => '.zhai',    //自定义模板文件后缀
+    //'TMPL_FILE_DEPR' => '_',    //连接符（防止模板目录太深,模板文件格式：'控制器名_方法名.模板后缀'）
+    //'VIEW_PATH' => './Theme/Views/',    //自定义视图目录：自定义模块视图文件目录
 );
--- Application/Views/Controller/IndexController.class.php
+++ Application/Views/Controller/IndexController.class.php
@@ -3,6 +3,27 @@ namespace Views\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->display();   //渲染视图
+        //定义变量
+        $username = 'zhai13';
+        $email = 'admin@13cloud.cn';
+        $age = 29;
+
+        //数组形式
+        $user = array(
+            'user' => $username,
+            'mail' => $email,
+            'age' => $age,
+        );
+        $this->assign('user',$user);
+
+        $fetchContent = $this->fetch(); //会返回渲染后的内容，但不会输出
+        $fetchContent = str_replace('zhai13', 'Thirteen', $fetchContent);
+        $this->show($fetchContent); //输出，一般是配合fetch方法使用
+        //使用assign方法为视图变量赋值; assign('变量名', 变量值);
+        /*$this->assign('user', $username);
+        $this->assign('mail', $email);
+        $this->assign('age', $age);*/
+
+        //$this->display();   //渲染视图
     }
 }
