7fe3b01a5923cbab6169ae698a2407e5107be42d
zhai13 <4254001@qq.com>
2016-04-07 09:46:57 +0800

    三（3）：视图主题功能
    1.配置默认主题名称：'DEFAULT_THEME'=>'default',
    2.配置自动侦测模板主题：'TMPL_DETECT_THEME'=>true,
    3.配置要切换的所有主题的名称：'THEME_LIST'=>'default,zhai13',
    
    在模板中用U方法组合切换模板的url，t：为默认模板切换变量，t的值为主题名称：{:U('index',array('t'=>'default'))}

--- Application/Views/Conf/config.php
+++ Application/Views/Conf/config.php
@@ -5,4 +5,7 @@ return array(
     //'TMPL_TEMPLATE_SUFFIX' => '.zhai',    //自定义模板文件后缀
     //'TMPL_FILE_DEPR' => '_',    //连接符（防止模板目录太深,模板文件格式：'控制器名_方法名.模板后缀'）
     //'VIEW_PATH' => './Theme/Views/',    //自定义视图目录：自定义模块视图文件目录
+    'DEFAULT_THEME'=>'default', //默认主题名称的配置
+    'TMPL_DETECT_THEME'=>true,  // 自动侦测模板主题
+    'THEME_LIST'=>'default,zhai13', //要切换的所有主题的名称
 );
--- Application/Views/Controller/IndexController.class.php
+++ Application/Views/Controller/IndexController.class.php
@@ -16,14 +16,14 @@ class IndexController extends Controller {
         );
         $this->assign('user',$user);
 
-        $fetchContent = $this->fetch(); //会返回渲染后的内容，但不会输出
-        $fetchContent = str_replace('zhai13', 'Thirteen', $fetchContent);
-        $this->show($fetchContent); //输出，一般是配合fetch方法使用
+        //$fetchContent = $this->fetch(); //会返回渲染后的内容，但不会输出
+        //$fetchContent = str_replace('zhai13', 'Thirteen', $fetchContent);
+        //$this->show($fetchContent); //输出，一般是配合fetch方法使用
         //使用assign方法为视图变量赋值; assign('变量名', 变量值);
         /*$this->assign('user', $username);
         $this->assign('mail', $email);
         $this->assign('age', $age);*/
 
-        //$this->display();   //渲染视图
+        $this->display();   //渲染视图
     }
 }
